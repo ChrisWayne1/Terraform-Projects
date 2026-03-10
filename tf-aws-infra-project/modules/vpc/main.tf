82add97035100114d1199cb2d82ce83004606a13
Chris4909 <chris.wayne4909@gmail.com>
2026-03-10 14:59:38 -0500

    add route table

--- tf-aws-infra-project/modules/vpc/main.tf
+++ tf-aws-infra-project/modules/vpc/main.tf
@@ -24,3 +24,19 @@ resource "aws_subnet" "private" {
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
 }
+
+#Create route table to receive traffic from internet
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
